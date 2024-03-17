a1cb10ba2ee1a3bf0aa65c9471dcb9e63ccb4c9f
rjgehan <rjgehan@loyola.edu>
2024-03-17 14:22:24 -0400

    test game

---
+++ ch1/games/ethane 4/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-Ethane-0.1";
+const contentToCache = [
+    "Build/ethane 4.loader.js",
+    "Build/ethane 4.framework.js",
+    "Build/ethane 4.data",
+    "Build/ethane 4.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
